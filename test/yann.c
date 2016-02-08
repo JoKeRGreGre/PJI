e531192a7ae37a12daac2dc164949b38d5155d7b
JoKeRGreGre <yann.moyart@gmail.com>
2016-02-08 16:29:24 +0100

    signaux ok, timer ok (taille d'un unsigned long ?)

--- test/yann.c
+++ test/yann.c
@@ -6,7 +6,8 @@
 
 void hello(){
 	int i;
-	for(i=1;i<=5;i++){
+	timer_start();
+	for(i=1;i<=10;i++){
 		printf("hello\n");
 		ptask_wait_for_period();
 	}
@@ -21,19 +22,18 @@ int main(){
 	/*Reglages des parametres de la tache*/
 	tpars params = TASK_SPEC_DFL;
 	params.period = tspec_from(1, SEC);
-	ptask_param_deadline(params,6,SEC);
+	ptask_param_deadline(params,5,SEC);
 	params.priority = 1;
 	params.act_flag = NOW;
 	params.measure_flag = 1;
 	printf("creating task\n");
 
 	int pid = ptask_create_param(hello,&params);
-	create_deadline_handler(pid,10,6000);
+	create_deadline_handler(pid,10,5000);
 	if (pid < 0) {
-		printf("Cannot create aperiodic task pid : %d\n",pid);
+		printf("Sudo ... : %d\n",pid);
 		exit(EXIT_FAILURE);
 	}
-	timer_start();
 	pthread_join(ptask_get_threadid(pid), 0);
 
 	return 0;
